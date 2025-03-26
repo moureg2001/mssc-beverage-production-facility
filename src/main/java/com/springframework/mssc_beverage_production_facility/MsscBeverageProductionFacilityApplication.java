5492d520b6ef9b77cee13cda6e1dbe7b6cd50dd0
firetor <moureg2001@gmail.com>
2025-03-26 22:56:17 +0100

    Project initializer

---
+++ src/main/java/com/springframework/mssc_beverage_production_facility/MsscBeverageProductionFacilityApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.mssc_beverage_production_facility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeverageProductionFacilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeverageProductionFacilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/mssc_beverage_production_facility/MsscBeverageProductionFacilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.mssc_beverage_production_facility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeverageProductionFacilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
