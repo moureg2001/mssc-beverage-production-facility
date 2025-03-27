fe835d795e2776938c44cd019f0f4cba841c4b3d
firetor <moureg2001@gmail.com>
2025-03-27 22:43:49 +0100

    remove unused import statement

--- src/main/java/com/springframework/mssc_beverage_production_facility/web/services/BeverageService.java
+++ src/main/java/com/springframework/mssc_beverage_production_facility/web/services/BeverageService.java
@@ -1,7 +1,6 @@
 package com.springframework.mssc_beverage_production_facility.web.services;
 
 import com.springframework.mssc_beverage_production_facility.web.model.BeverageDto;
-import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
